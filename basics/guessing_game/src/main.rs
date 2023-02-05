860b97db01d5dcaf3ba6f7e73875e44087b8aff6
Visha <diegojosevilla@outlook.com>
2023-02-05 15:05:10 -0600

    guessing_game: ask for user input

--- basics/guessing_game/src/main.rs
+++ basics/guessing_game/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
+    println!("------------------------------");
     println!("Welcome to the guessing game!");
+    println!("------------------------------");
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line!");
+
+    println!("Your guess is: {}", guess);
 }
