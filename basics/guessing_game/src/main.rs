975d4f8f98053de4e9625e213b8de0ae2565dca5
Visha <diegojosevilla@outlook.com>
2023-02-05 15:25:30 -0600

    guessing_game: add loop until user ends the game

--- basics/guessing_game/src/main.rs
+++ basics/guessing_game/src/main.rs
@@ -8,20 +8,28 @@ fn main() {
 
     let secret_num: i32 = rand::thread_rng().gen_range(1..101);
 
-    println!("Please input your guess:");
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess:");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("Your guess is: {}", guess);
+        println!("Your guess is: {}", guess);
 
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big"),
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small! \n"),
+            Ordering::Greater => println!("Too big \n"),
+            Ordering::Equal => {
+                println!("You win! \n");
+                break;
+            }
+        }
     }
 }
